6bee94fcea0e607af2949589a15e7aa51d734087
Emily Haag <emilyannehaag@gmail.com>
2018-08-27 18:09:25 -0600

    Assignemnt Topic Resource Pt 2 Completed

--- src/routes/advertisements.js
+++ src/routes/advertisements.js
@@ -5,6 +5,10 @@ const advertisementController = require("../controllers/advertisementController"
 
 router.get("/advertisements", advertisementController.index);
 router.get("/advertisements/new", advertisementController.new);
+router.get("/advertisements/:id", advertisementController.show);
+router.get("/advertisements/:id/edit", advertisementController.edit);
 router.post("/advertisements/create", advertisementController.create);
+router.post("/advertisements/:id/update", advertisementController.update);
+router.post("/advertisements/:id/destroy", advertisementController.destroy);
 
 module.exports = router;
